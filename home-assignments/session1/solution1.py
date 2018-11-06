906e808e5f767ef4f1d0ed0e20deb7b5aa530122
aviadp <Elbit-99>
2018-11-06 16:31:17 +0200

    added args, changed results from string to dict, for yaml proper display known issue: encoding file to avoid unknown chars in results

--- home-assignments/session1/solution1.py
+++ home-assignments/session1/solution1.py
@@ -1,28 +1,14 @@
 import json
 import yaml
+import argparse
 
 
-# TODO: add file as argument
-# TODO: create function that print results into yaml file
-
-
-def draft():
-    j = "input.json"
-
-    with open("input.json") as jfile:
-        output = json.load(jfile)
-
-    ppl_ages = output['ppl_ages']
-    buckets = output['buckets']
-    print(ppl_ages, "\n", buckets)
-
-    for key, value in ppl_ages.items():
-        print(key)
+# TODO: fix encoding issue
 
 
 def import_data_from_file(file_name):
     j_file = file_name
-    with open(j_file) as jfile:
+    with open(j_file, encoding="utf-8") as jfile:
         output = json.load(jfile)
 
     ppl_ages = output['ppl_ages']
@@ -42,31 +28,37 @@ def import_data_from_file(file_name):
     return results
 
 
-def sort_data(data):
+def sort_output_data(data):
     buckets_list = data[1]
     pairs = []
     for i in range(0, len(buckets_list) - 1):
         pairs.append([buckets_list[i], buckets_list[i + 1]])
 
-    results_string = ""
+    result_dict = {}
     for pair in pairs:
-        results_string = results_string + "{0}-{1}: \n".format(pair[0], pair[1])
+        ages = "{0} - {1}".format(pair[0], pair[1])
+        result_dict[ages] = []
         for key, value in data[0].items():
             if (min(pair)) <= value < (max(pair)):
-                results_string = results_string + "- {0} \n".format(key)
+                result_dict[ages].append(key)
 
-    print(results_string)
-    return results_string
-
-
-def write_to_ymal(results_dict):
-    pass
+    return result_dict
 
 
-def main():
-    file_name = "input.json"
-    sort_data(import_data_from_file(file_name))
+def write_to_yaml(sorted_dict):
+    with open("ex1_results.yaml", "w") as yaml_file:
+        yaml.dump(sorted_dict, yaml_file, default_flow_style=False)
+
+
+def main(input_file: str):
+    sorted_data = import_data_from_file(input_file)
+    data_output = sort_output_data(sorted_data)
+    write_to_yaml(data_output)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-i", "--input", help="JSON file path")
+    args = parser.parse_args()
+    input_file = args.input
+    main(input_file=input_file)
