60c647fed1e0a67d7dfdec92c0246b4f8f1e8c7a
aviadp <Elbit-99>
2018-11-07 12:42:26 +0200

    Finished to write working program. some TODO still open

--- home-assignments/session1/solution2.py
+++ home-assignments/session1/solution2.py
@@ -2,28 +2,9 @@ import requests
 import json
 
 
-def draft():
-    """
-    location_by_ip_api = 'http://ip-api.com/json'
-
-    weather_api = 'http://api.openweathermap.org/data/2.5/weather?q={city},{country}&APPID=07de467d22717f0eeee1c32550a7ebb1
-    """
-    url = "http://ip-api.com/json"
-    my_location = requests.get(url=url)
-    my_location = my_location.json()
-    # print(my_location)
-
-    my_city = my_location['city']
-    my_country = my_location['country']
-
-    # print("{0} ,{1}".format(my_city, my_country))
-
-    weather_url = "http://api.openweathermap.org/data/2.5/weather?q={city},{country}&units=metric&APPID=07de467d22717f0eeee1c32550a7ebb1".format(
-        city=my_city, country=my_country)
-    weather = requests.get(url=weather_url)
-    weather = weather.json()
-
-    print(weather)
+# TODO: documantation
+# TODO: add try / except in requests part.
+# TODO: add try / except in writing file function
 
 
 def check_self_location_by_ip():
@@ -54,9 +35,34 @@ def write_to_file(data, file_name):
         exit(0)
 
 
+def parse_temp_from_weather(weather):
+    temp = weather["main"]["temp"]
+    name = weather["name"]
+    temp_w_name = (temp, name)
+
+    return temp_w_name
+
+
+def display_multiple_cities_weather(cities_list: list):
+    for place in cities_list:
+        weather = get_weather_by_location(place)
+        temp_w_name = parse_temp_from_weather(weather)
+        if temp_w_name[1] == place[0]:
+            line = "The weather in {city}, {country} is {temp} degrees\n".format(city=place[0], country=place[1],
+                                                                                 temp=temp_w_name[0])
+            print(line)
+        else:
+            print("{city} name is not correct\n".format(city=place[0]))
+
+
 def main():
+    cities_list = [("Tel Aviv", "Israel"), ("Haifa", "Israel"), ("Jerusalem", "Israel"), ("Rehovot", "Israel"),
+                   ("Beer Sheva", "Israel"), ("London", "England"), ("Helsinki", "Finland"), ("Munich", "Germany"),
+                   ("Paris", "French"), ("Frankfurt", "Germany")]
     location = check_self_location_by_ip()
     weather = get_weather_by_location(location)
+    parse_temp_from_weather(weather)
+    display_multiple_cities_weather(cities_list)
     write_to_file(weather, "weather.txt")
 
 
