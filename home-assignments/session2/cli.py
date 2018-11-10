6671e29fb8ab9f1b1d58f95c07a8edbe92e3c63b
aviadP <aviad.polak@gmail.com>
2018-11-10 22:56:39 +0200

    added 2 functions: get_weather_info_by_city forecast_by_weather_info

--- home-assignments/session2/cli.py
+++ home-assignments/session2/cli.py
@@ -1,8 +1,9 @@
 from weather import Weather, Unit
+import click
+
 
 def draft():
-
-    temp_units = input("select required temperture units, type 1 for celsius, 2 for Fahrenheit ")
+    temp_units = input("select required temperature units, type 1 for celsius, 2 for Fahrenheit ")
     if temp_units == "1":
         weather = Weather(unit=Unit.CELSIUS)
         selected_units = "CELSIUS"
@@ -20,8 +21,48 @@ def draft():
     high = city_weather_info.forecast[0].high
     low = city_weather_info.forecast[0].low
 
-    announcement = "the temperture in {0} today is {1} with temperture trailing from {2}-{3} {4}".format(city, condition,
-                                                                                                         high, low,
-                                                                                                         selected_units)
+    announcement = "the temperature in {0} today is {1} with temperature trailing from {2}-{3} {4}".format(city,
+                                                                                                           condition,
+                                                                                                           high, low,
+                                                                                                           selected_units)
 
     print(announcement)
+
+
+def get_weather_info_by_city(city):
+    weather = Weather(unit=Unit.CELSIUS)
+
+    city_weather_info = weather.lookup_by_location(city)
+
+    return city_weather_info
+
+
+def forecast_by_weather_info(city_weather_info):
+    forecasts = city_weather_info.forecast
+    condition = []
+    high = []
+    low = []
+    for forecast in forecasts:
+        condition.append(forecast.text)
+        high.append(forecast.high)
+        low.append(forecast.low)
+
+    full_forecast_info = {"condition": condition, "high": high, "low": low}
+    print(full_forecast_info)
+
+    return full_forecast_info
+
+
+
+
+
+@click.command()
+@click.option("--city", help="selected city")
+@click.option("--forecast", help="selected days , starting from today"
+                                 "for example: --forecast TODAY for today's forecast, TODAY+3 for next 3 days(including today)")
+def main(data):
+    pass
+
+
+city_info = get_weather_info_by_city("Tel Aviv")
+print(forecast_by_weather_info(city_info))
