65600ee246a2c6e6c12d394640379071350c544b
aviadP <aviad.polak@gmail.com>
2018-11-20 20:56:47 +0200

    in progress

--- home-assignments/session2/cli.py
+++ home-assignments/session2/cli.py
@@ -1,42 +1,20 @@
-from weather import Weather, Unit
-import click
-
-"""def draft():
-    temp_units = input("select required temperature units, type 1 for celsius, 2 for Fahrenheit ")
-    if temp_units == "1":
-        weather = Weather(unit=Unit.CELSIUS)
-        selected_units = "CELSIUS"
-    elif temp_units == "2":
-        weather = Weather(unit=Unit.FAHRENHEIT)
-        selected_units = "FAHRENHEIT"
-    else:
-        print("wrong input")
-        exit(1)
-
-    city = 'oslo'
-    city_weather_info = weather.lookup_by_location(city)
-
-    condition = city_weather_info.forecast[0].text
-    high = city_weather_info.forecast[0].high
-    low = city_weather_info.forecast[0].low
-
-    announcement = "the temperature in {0} today is {1} with temperature trailing from {2}-{3} {4}".format(city,
-                                                                                                           condition,
-                                                                                                           high, low,
-                                                                                                           selected_units)
-
-    print(announcement)
-
-"""
+try:
+    import click
+    from weather import Weather, Unit
+except Exception as e:
+    e.KeyError: ModuleNotFoundError
 
 
 def get_weather_info_by_city(city, units):
     if units == "c":
         weather = Weather(unit=Unit.CELSIUS)
-        selected_units = "CELSIUS"
+
     elif units == "f":
         weather = Weather(unit=Unit.FAHRENHEIT)
-        selected_units = "FAHRENHEIT"
+
+    else:
+        print("none or wrong units selected")
+        exit(1)
 
     city_weather_info = weather.lookup_by_location(city)
 
@@ -54,18 +32,23 @@ def forecast_by_weather_info(city_weather_info):
         low.append(forecast.low)
 
     full_forecast_info = {"condition": condition, "high": high, "low": low}
-    print(full_forecast_info)
 
     return full_forecast_info
 
 
 @click.command()
 @click.option("--city", help="selected city")
-@click.option("--forecast", help="selected days , starting from today"
-                                 "for example: --forecast TODAY for today's forecast, TODAY+3 for next 3 days")
-def main(data):
-    pass
+@click.option("--units", type=click.Choice(['c', 'f']), help="select 'c' for Celsius or 'f' for Fahrenhiet.")
+@click.option("--forecast", default=0, help="selected days , starting from today"
+                                            "for example: --forecast TODAY for today's forecast, TODAY+3 for next 3 days")
+def main(city, units, forecast):
+    print(forecast)
+    city_weather_info = get_weather_info_by_city(city, units)
+    full_forecast_info = forecast_by_weather_info(city_weather_info)
+    output = [full_forecast_info.get("condition"), full_forecast_info.get("high"), full_forecast_info.get("low")]
+    for i in output:
+        print(i[0])
 
 
-city_info = get_weather_info_by_city("Tel Aviv")
-print(forecast_by_weather_info(city_info))
+if __name__ == "__main__":
+    main()
