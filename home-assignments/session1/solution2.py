c6b6d8eddd2e91b1971d36f534e95c81b5a39175
aviadp <Elbit-99>
2018-11-25 17:33:51 +0200

    fixed code review comments

--- home-assignments/session1/solution2.py
+++ home-assignments/session1/solution2.py
@@ -2,15 +2,14 @@ import requests
 import json
 
 
-# TODO: documantation
-# TODO: add try / except in requests part.
-# TODO: add try / except in writing file function
-
-
 def check_self_location_by_ip():
     ip_api_url = "http://ip-api.com/json"
-    my_location = requests.get(url=ip_api_url)
-    my_location = my_location.json()
+    try:
+        my_location = requests.get(url=ip_api_url)
+        my_location = my_location.json()
+    except Exception as e:
+        print(e)
+        exit(1)
     my_city = my_location['city']
     my_country = my_location['country']
     location = (my_city, my_country)
@@ -21,18 +20,26 @@ def check_self_location_by_ip():
 def get_weather_by_location(location):
     city = location[0]
     country = location[1]
-    weather_url = "http://api.openweathermap.org/data/2.5/weather?q={city},{country}&units=metric&APPID=07de467d22717f0eeee1c32550a7ebb1".format(
-        city=city, country=country)
-    weather = requests.get(url=weather_url)
-    weather = weather.json()
+    try:
+        weather_url = "http://api.openweathermap.org/data/2.5/weather?q={city}," \
+                      "{country}&units=metric&APPID=07de467d22717f0eeee1c32550a7ebb1".format(
+            city=city, country=country)
+        weather = requests.get(url=weather_url)
+        weather = weather.json()
+    except Exception as e:
+        print(e)
+        exit(0)
 
     return weather
 
 
 def write_to_file(data, file_name):
-    with open(file_name, "w", encoding="utf-8") as f:
-        f.write(json.dumps(data))
-        exit(0)
+    try:
+        with open(file_name, "w", encoding="utf-8") as f:
+            f.write(json.dumps(data))
+    except Exception as e:
+        print(e)
+        exit(1)
 
 
 def parse_temp_from_weather(weather):
